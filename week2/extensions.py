a807c3dfdee9ca1fe3a00e4604e0bf0f94cf5b6c
Abdelrahman-Gaber <a.gaber.khalifa@gmail.com>
2026-08-28 01:40:00 +0300

    3rd problem in lecture 3

---
+++ week2/extensions.py
@@ -0,0 +1,15 @@
+file_name = input("enter the file name: ")
+
+match file_name.split(".")[-1].lower():
+    case "gif":
+        print("image/gif")
+    case "jpg" | "jpeg":
+        print("image/jpeg")
+    case "png":
+        print("image/png")
+    case "pdf":
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
